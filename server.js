df891e9d127d9aec26399a35edf44c3478cd4c69
onehattoomany <31289469+onehattoomany@users.noreply.github.com>
2017-08-30 14:55:41 -0500

    Update server.js

--- server.js
+++ server.js
@@ -76,6 +76,24 @@ app.get('/', function (req, res) {
   }
 });
 
+app.get('/rss', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      res.render('sample.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('sample.html', { pageCountMessage : null});
+  }
+});
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
